70afac85c5c8d63cf65d182d125177e69b691214
zsnydr <zackwsnyder@gmail.com>
2016-12-13 17:13:03 -0800

    [add] build initial webpack setup

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('./webpack.config');
+const webpackHot = require('webpack-hot-middleware');
+
 const app = express();
+
+const compiler = webpack(webpackConfig);
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(path.join(__dirname, '/publicServed')));
+// app.use('/assets', express.static(path.join(__dirname, '/assets')));
+
+app.use(webpackMiddleware(compiler, {
+  publicPath: webpackConfig.output.publicPath
+}));
+
+app.use(webpackHot(compiler, {
+  log: console.log,
+  path: '/__webpack_hmr',
+  heartbeat: 1 * 1000
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('hello');
-});
 
 app.listen(app.get('port'));
 console.log('Listening to port... ', app.get('port'));
