3b7296d6d5bd2d823422b6559de76415be20c6f3
zsnydr <zackwsnyder@gmail.com>
2016-12-13 16:47:01 -0800

    [add] build basic server that serves 'hello'

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(app.get('port'));
+console.log('Listening to port... ', app.get('port'));
+
+module.exports = app;
