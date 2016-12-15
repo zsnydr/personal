8c3303466c2b18d4ccb758ad66ddb51ee11527bd
zsnydr <zackwsnyder@gmail.com>
2016-12-14 17:28:58 -0800

    [add] build basic client react-routing, dummy components in index.jsx

--- public/index.jsx
+++ public/index.jsx
@@ -1,8 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Router, IndexRoute, browserHistory } from 'react-router';
+
+const Main = (props) => {
+  return (
+    <div>
+      <Nav />
+      {props.children}
+      <Footer />
+    </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <div>NAV HERE</div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>FOOTER HERE</div>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <div onClick={() => { browserHistory.push('/about'); }}>TO ABOUT</div>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <div>ABOUT ME</div>
+      <div onClick={() => { browserHistory.push('/'); }}>BACK HOME</div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={Home} />
+        <Router path="/about" component={About} />
+      </Route>
+    </Router>
+  );
+};
+
 
 ReactDOM.render(
-  <div>HELLO</div>
-  ,
+  <App />,
   document.querySelector('.main')
 );
