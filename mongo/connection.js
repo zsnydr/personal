5517f18aee2ab4ab8e6c68a4c02c91cc13967da7
zsnydr <zackwsnyder@gmail.com>
2017-01-31 17:33:07 -0800

    [fix] change MONGOLAB to MONGODB env variable

--- mongo/connection.js
+++ mongo/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const CONNECTION_URI = process.env.MONGOLAB_URI || 'mongodb://localhost/personal';
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost/personal';
 
 mongoose.connect(CONNECTION_URI);
 
